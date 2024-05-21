c9999890f00eb695529384bcab124c5251418264
Dihep <dihep.demayo@gmail.com>
2024-05-22 00:33:02 +0200

    M1 - Mail generate e salvate

--- js/script.js
+++ js/script.js
@@ -3,13 +3,20 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            mail: ``,
-            indirizzo: `https://flynn.boolean.careers/exercises/api/random/int`
+            mailList: [],
+            indirizzo: `https://flynn.boolean.careers/exercises/api/random/mail`
         }
     },
     methods: {
-        test() {
-            console.log(axios.get(this.indirizzo));
+        generaMails() {
+            for (let i = 0; i < 9; i++) {
+                let mail;
+                axios.get(this.indirizzo).then((result) => {
+                 mail = result.data.response;
+                 this.mailList.push(mail); 
+                });
+            };
+            console.log(this.mailList)
         }
     }
 }).mount('#vueApp');
